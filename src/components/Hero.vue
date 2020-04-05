07f629416918ca6c8aa5462a4e846408795b3f53
miracFence <themiracfence.code@gmail.com>
2020-04-05 16:27:03 -0500

    Added a home page

---
+++ src/components/Hero.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hero is-medium is-primary-is-bold">
+    <div class="container h-100">
+      <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-md-5">
+          <div class="hero-content">
+            <h1 class="hero-title">Your favorite digital art shop!</h1>
+            <p>Explore our different art options that our artists create for you</p>
+            <div class="hero-btn mt-5">
+              <button class="btn custom-btn btn-info mr-4">Explore</button>
+              <button class="btn custom-btn btn-outline-secondary">Products</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-7">
+          <div class="graphic">
+            <img src="/img/svg/graphic.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hero {
+  padding-top: 7rem;
+  width: 100%;
+  height: 500px;
+  text-align: left;
+}
+</style>
